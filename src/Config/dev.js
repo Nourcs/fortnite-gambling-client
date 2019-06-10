0d8b9203f35815ec96bc4fe82a4a63889ba5a425
Nour Essoussi <nourcherifsoussi@gmail.com>
2019-06-09 23:54:56 -0400

    Deleted DevJS

--- src/Config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apiKey: "AIzaSyDr5aaDEeojwZt0PO27vAyHslxwO_dBOl4",
-  authDomain: "fortnite-gambling.firebaseapp.com",
-  databaseURL: "https://fortnite-gambling.firebaseio.com",
-  projectId: "fortnite-gambling",
-  storageBucket: "fortnite-gambling.appspot.com",
-  messagingSenderId: "540987258265",
-  appId: "1:540987258265:web:6f678251065d5440"
-};
